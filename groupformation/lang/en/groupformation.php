087d1e8ca841c2016edc85a8f6ba940cd074edc4
Nora Wester <nora_emw@gmx.de>
2014-12-12 15:11:32 +0100

    lang en folder added

---
+++ groupformation/lang/en/groupformation.php
@@ -0,0 +1,40 @@
+<?php
+
+	// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+/**
+* English strings for newmodule
+*
+* @package mod_groupformation
+* @copyright 2014 Nora Wester
+* @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+	defined('MOODLE_INTERNAL') || die();
+
+	$string['modulename'] = 'groupformation';
+	$string['modulenameplural'] = 'groupformations';
+	$string['modulename_help'] = 'With the groupformation plugin you can generate learning groups';
+	$string['groupformationfieldset'] = 'Custom example fieldset';
+	$string['groupformationname'] = 'Groupformation';
+	$string['groupformationname_help'] = 'ToolTip Groupformation';
+	$string['groupformation'] = 'Groupformation';
+	$string['pluginadministration'] = 'Administration for Groupformation';
+	$string['pluginname'] = 'groupformation';
+	$string['nonewmodules'] = '';
+	$string['groupnameexists'] = 'This group name already exist';
+	$string['generategroups'] = 'generate groups';
+	$string['namingschema'] = "namingchema";
+	$string['userpergroup'] = 'How many users per group?';
