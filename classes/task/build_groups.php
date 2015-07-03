b6ddfded1025d7b87afcbfa21bfef552039a9fef
Rene Roepke <roepkix@googlemail.com>
2015-07-03 18:24:55 +0200

    cleaning up

--- classes/task/build_groups.php
+++ classes/task/build_groups.php
@@ -6,12 +6,10 @@ require_once ($CFG->dirroot . '/mod/groupformation/locallib.php');
 class build_groups extends \core\task\scheduled_task {
 	public function get_name() {
 		// Shown in admin screens
-		return "test_groupformation_task";
+		return "groupformation_job_build_groups";
 	}
 	 
 	public function execute() {
-		groupformation_log(1,1,"<update_instance>");
 		sleep(180);
-		groupformation_log(1,1,"<view_settings>");
 	}
 }
