9b474dd26841d4414dd8b635772923acf0f2fff1
Rene Roepke <roepkix@googlemail.com>
2015-08-01 21:53:53 +0200

    change task

--- classes/task/build_groups.php
+++ classes/task/build_groups.php
@@ -1,5 +1,27 @@
 <?php
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Task - Cron
+ *
+ * @package mod_groupformation
+ * @author Rene & Ahmed
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 namespace mod_groupformation\task;
 
 require_once ($CFG->dirroot . '/mod/groupformation/locallib.php');
@@ -58,7 +80,6 @@ class build_groups extends \core\task\scheduled_task {
 				
 				$saved = \mod_groupformation_job_manager::save_result ( $job, $groupal_cohort, $result [1], $result [2] );
 			}
-			
 		}
 		return $saved;
 	}
