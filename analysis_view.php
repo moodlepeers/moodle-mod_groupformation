795ef223640b01cf18576e917a168d346c99dd54
Rene Roepke <roepkix@googlemail.com>
2015-08-04 12:30:59 +0200

    improve test user generation

--- analysis_view.php
+++ analysis_view.php
@@ -27,13 +27,17 @@ require_once (dirname ( __FILE__ ) . '/locallib.php');
 
 /* Ahmed */
 
-require_once (dirname ( __FILE__ ) . '/classes/moodle_interface/create_q_testuser.php');
+require_once (dirname ( __FILE__ ) . '/classes/moodle_interface/test_user_generator.php');
 require_once (dirname ( __FILE__ ) . '/classes/moodle_interface/storage_manager.php');
 
 // Read URL params
 $id = optional_param ( 'id', 0, PARAM_INT ); // Course Module ID
                                              // $g = optional_param ( 'g', 0, PARAM_INT ); // groupformation instance ID
 $do_show = optional_param ( 'do_show', 'analysis', PARAM_TEXT );
+
+$create_users = optional_param ( 'create_users', 0, PARAM_INT );
+$create_answers = optional_param ( 'create_answers', false, PARAM_BOOL );
+$delete_users = optional_param ( 'delete_users', false, PARAM_BOOL );
 
 // Import jQuery and js file
 groupformation_add_jquery ( $PAGE, 'survey_functions.js' );
@@ -79,7 +83,7 @@ require ('tabs.php');
 
 // Conditions to show the intro can change to look for own settings or whatever.
 // if ($groupformation->intro) {
-// 	echo $OUTPUT->box ( format_module_intro ( 'groupformation', $groupformation, $cm->id ), 'generalbox mod_introbox', 'groupformationintro' );
+// echo $OUTPUT->box ( format_module_intro ( 'groupformation', $groupformation, $cm->id ), 'generalbox mod_introbox', 'groupformationintro' );
 // }
 
 // Replace the following lines with you own code.
@@ -100,19 +104,29 @@ if ($_POST) {
 	}
 }
 
-/* ---------- Ahmed Testet User creation ---------- */
+/* ---------- Automated test user generation ---------- */
 
-$cqt = new mod_groupformation_create_q_testuser ();
+$cqt = new mod_groupformation_test_user_generator ();
 
-/*
- * @param1: number of testuser
- * @param2: groupformation id
- */
-
-// $cqt->createTestusers ( 60, $groupformation->id,true);
-// $cqt->deleteTestusers();
-
-/* ---------- / Ahmed Tested User creation ---------- */
+if ($delete_users){
+	$cqt->delete_test_users ( $groupformation->id );
+	$returnurl = new moodle_url ( '/mod/groupformation/analysis_view.php', array (
+			'id' => $id,
+			'do_show' => 'analysis'
+	) );
+	redirect ( $returnurl );
+	
+}
+if ($create_users > 0){
+	$cqt->create_test_users ( $create_users, $groupformation->id,$create_answers);
+	$returnurl = new moodle_url ( '/mod/groupformation/analysis_view.php', array (
+			'id' => $id,
+			'do_show' => 'analysis'
+	) );
+	redirect ( $returnurl );
+}
+	
+/* ---------- / Automated test user generation ---------- */
 
 $jm = new mod_groupformation_job_manager ();
 
@@ -125,8 +139,8 @@ $incomplete_cohort = null;
 $job = $jm::get_job ( $groupformation->id );
 
 if (! is_null ( $job )) {
-// 	$result = $jm::do_groupal ( $job, $groupal_cohort, $random_cohort, $incomplete_cohort);
-// 	var_dump($result);
+	// $result = $jm::do_groupal ( $job, $groupal_cohort, $random_cohort, $incomplete_cohort);
+	// var_dump($result);
 }
 // -----------------------------------------------
 
