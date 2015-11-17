15be19541c10cb2e25c76f1c3e16d34f5f6e24cd
Rene Roepke <roepkix@googlemail.com>
2015-11-17 23:23:55 +0100

    reformatted class to moodle coding style conformance (90%)

--- classes/controller/analysis_controller.php
+++ classes/controller/analysis_controller.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -9,38 +8,44 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
-
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
 /**
  * Controller for analysis view
- * 
- * @package mod_groupformation
- * @author MoodlePeers
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @package    mod_groupformation
+ * @copyright  2015 MoodlePeers
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-if (! defined( 'MOODLE_INTERNAL' )) {
-    die( 'Direct access to this script is forbidden.' );
-}
+defined( 'MOODLE_INTERNAL' ) || die( 'Direct access to this script is forbidden.' );
 
-require_once ($CFG->dirroot . '/mod/groupformation/classes/moodle_interface/storage_manager.php');
-require_once ($CFG->dirroot . '/mod/groupformation/classes/moodle_interface/user_manager.php');
-require_once ($CFG->dirroot . '/mod/groupformation/classes/util/template_builder.php');
-require_once ($CFG->dirroot . '/mod/groupformation/classes/util/util.php');
+require_once($CFG->dirroot . '/mod/groupformation/classes/moodle_interface/storage_manager.php');
+require_once($CFG->dirroot . '/mod/groupformation/classes/moodle_interface/user_manager.php');
+require_once($CFG->dirroot . '/mod/groupformation/classes/util/template_builder.php');
+require_once($CFG->dirroot . '/mod/groupformation/classes/util/util.php');
 class mod_groupformation_analysis_controller {
+    
+    /** @var int The id of the groupformation activity */
     private $groupformationid;
     private $cm;
+    private $jobstate;
+    
+    /** @var mod_groupformation_storage_manager The manager of activity data */
     private $store = null;
-    private $user_manager;
+    
+    /** @var mod_groupformation_user_manager The manager of user data */
+    private $usermanager;
+    
     private $view = null;
-    private $questionnaire_available;
-    private $activity_time;
-    private $start_time;
-    private $end_time;
-    private $time_now;
+    private $questionnaireavailable;
+    private $activitytime;
+    private $starttime;
+    private $endtime;
+    private $timenow;
     private $test;
     private $state;
     
@@ -52,11 +57,9 @@ class mod_groupformation_analysis_controller {
     public function __construct($groupformationid, $cm) {
         $this->cm = $cm;
         $this->groupformationid = $groupformationid;
-        
         $this->store = new mod_groupformation_storage_manager( $groupformationid );
-        $this->user_manager = new mod_groupformation_user_manager( $groupformationid );
+        $this->usermanager = new mod_groupformation_user_manager( $groupformationid );
         $this->view = new mod_groupformation_template_builder();
-        
         $this->determine_status();
     }
     
@@ -80,59 +83,60 @@ class mod_groupformation_analysis_controller {
      * @return string
      */
     private function load_status() {
-        $statusAnalysisView = new mod_groupformation_template_builder();
-        $statusAnalysisView->set_template( 'analysis_status' );
+        $statusanalysisview = new mod_groupformation_template_builder();
+        $statusanalysisview->set_template( 'analysis_status' );
         
-        $this->activity_time = $this->store->get_time();
+        $this->activitytime = $this->store->get_time();
         
-        if (intval( $this->activity_time ['start_raw'] ) == 0) {
-            $this->start_time = get_string( 'no_time', 'groupformation' );
+        if (intval( $this->activitytime ['start_raw'] ) == 0) {
+            $this->starttime = get_string( 'no_time', 'groupformation' );
         } else {
-            $this->start_time = $this->activity_time ['start'];
+            $this->starttime = $this->activitytime ['start'];
         }
         
-        if (intval( $this->activity_time ['end_raw'] ) == 0) {
-            $this->end_time = get_string( 'no_time', 'groupformation' );
+        if (intval( $this->activitytime ['end_raw'] ) == 0) {
+            $this->endtime = get_string( 'no_time', 'groupformation' );
         } else {
-            $this->end_time = $this->activity_time ['end'];
+            $this->endtime = $this->activitytime ['end'];
         }
         
-        $button_name = ($this->questionnaire_available) ? "stop_questionnaire" : "start_questionnaire";
-        $button_caption = ($this->questionnaire_available) ? get_string( 'activity_end', 'groupformation' ) : get_string( 'activity_start', 'groupformation' );
-        $button_disabled = ($this->job_state !== "ready") ? "disabled" : "";
+        $buttonname = ($this->questionnaireavailable) ? "stop_questionnaire" : "start_questionnaire";
+        $buttoncaption = ($this->questionnaireavailable) ? get_string( 'activity_end', 'groupformation' ) :
+                get_string( 'activity_start', 'groupformation' );
+        $buttondisabled = ($this->jobstate !== "ready") ? "disabled" : "";
         
-        $statusAnalysisView->assign( 'button', array (
+        $statusanalysisview->assign( 'button', array (
                 'type' => 'submit',
-                'name' => $button_name,
+                'name' => $buttonname,
                 'value' => '',
-                'state' => $button_disabled,
-                'text' => $button_caption 
+                'state' => $buttondisabled,
+                'text' => $buttoncaption,
         ) );
         
-        $info_teacher = mod_groupformation_util::get_info_text_for_teacher( false, "analysis" );
+        $infoteacher = mod_groupformation_util::get_info_text_for_teacher( false, "analysis" );
         
-        $statusAnalysisView->assign( 'info_teacher', $info_teacher );
-        $statusAnalysisView->assign( 'analysis_time_start', $this->start_time );
-        $statusAnalysisView->assign( 'analysis_time_end', $this->end_time );
+        $statusanalysisview->assign( 'info_teacher', $infoteacher );
+        $statusanalysisview->assign( 'analysis_time_start', $this->starttime );
+        $statusanalysisview->assign( 'analysis_time_end', $this->endtime );
         
         switch ($this->state) {
             case 1 :
-                $statusAnalysisView->assign( 'analysis_status_info', get_string( 'analysis_status_info0', 'groupformation' ) );
+                $statusanalysisview->assign( 'analysis_status_info', get_string( 'analysis_status_info0', 'groupformation' ) );
                 break;
             case 2 :
-                $statusAnalysisView->assign( 'analysis_status_info', get_string( 'analysis_status_info1', 'groupformation' ) );
+                $statusanalysisview->assign( 'analysis_status_info', get_string( 'analysis_status_info1', 'groupformation' ) );
                 break;
             case 3 :
-                $statusAnalysisView->assign( 'analysis_status_info', get_string( 'analysis_status_info2', 'groupformation' ) );
+                $statusanalysisview->assign( 'analysis_status_info', get_string( 'analysis_status_info2', 'groupformation' ) );
                 break;
             case 4 :
-                $statusAnalysisView->assign( 'analysis_status_info', get_string( 'analysis_status_info4', 'groupformation' ) );
+                $statusanalysisview->assign( 'analysis_status_info', get_string( 'analysis_status_info4', 'groupformation' ) );
                 break;
             default :
-                $statusAnalysisView->assign( 'analysis_status_info', get_string( 'analysis_status_info3', 'groupformation' ) );
+                $statusanalysisview->assign( 'analysis_status_info', get_string( 'analysis_status_info3', 'groupformation' ) );
         }
         
-        return $statusAnalysisView->load_template();
+        return $statusanalysisview->load_template();
     }
     
     /**
@@ -143,20 +147,20 @@ class mod_groupformation_analysis_controller {
     private function load_statistics() {
         global $PAGE;
         
-        $questionnaire_StatisticNumbers = mod_groupformation_util::get_infos( $this->groupformationid );
+        $questionnairestats = mod_groupformation_util::get_infos( $this->groupformationid );
         
-        $statisticsAnalysisView = new mod_groupformation_template_builder();
-        $statisticsAnalysisView->set_template( 'analysis_statistics' );
+        $statisticsanalysisview = new mod_groupformation_template_builder();
+        $statisticsanalysisview->set_template( 'analysis_statistics' );
         $context = $PAGE->context;
         $count = count( get_enrolled_users( $context, 'mod/groupformation:onlystudent' ) );
         
-        $statisticsAnalysisView->assign( 'statistics_enrolled', $questionnaire_StatisticNumbers [0] );
-        $statisticsAnalysisView->assign( 'statistics_processed', $questionnaire_StatisticNumbers [1] );
-        $statisticsAnalysisView->assign( 'statistics_submited', $questionnaire_StatisticNumbers [2] );
-        $statisticsAnalysisView->assign( 'statistics_submited_incomplete', $questionnaire_StatisticNumbers [4] );
-        $statisticsAnalysisView->assign( 'statistics_submited_complete', $questionnaire_StatisticNumbers [3] );
+        $statisticsanalysisview->assign( 'statistics_enrolled', $questionnairestats [0] );
+        $statisticsanalysisview->assign( 'statistics_processed', $questionnairestats [1] );
+        $statisticsanalysisview->assign( 'statistics_submited', $questionnairestats [2] );
+        $statisticsanalysisview->assign( 'statistics_submited_incomplete', $questionnairestats [4] );
+        $statisticsanalysisview->assign( 'statistics_submited_complete', $questionnairestats [3] );
         
-        return $statisticsAnalysisView->load_template();
+        return $statisticsanalysisview->load_template();
     }
     
     /**
@@ -177,7 +181,7 @@ class mod_groupformation_analysis_controller {
      */
     public function determine_status() {
         global $DB;
-        $this->questionnaire_available = $this->store->is_questionnaire_available();
+        $this->questionnaireavailable = $this->store->is_questionnaire_available();
         $this->state = 1;
         $job = mod_groupformation_job_manager::get_job( $this->groupformationid );
         if (is_null( $job )) {
@@ -185,14 +189,12 @@ class mod_groupformation_analysis_controller {
             mod_groupformation_job_manager::create_job( $this->groupformationid, $groupingid );
             $job = mod_groupformation_job_manager::get_job( $this->groupformationid );
         }
-        $this->job_state = mod_groupformation_job_manager::get_status( $job );
-        $completed_q = count( $this->user_manager->get_completed() );
-        
-        if ($this->job_state !== 'ready') {
+        $this->jobstate = mod_groupformation_job_manager::get_status( $job );
+        if ($this->jobstate !== 'ready') {
             $this->state = 3;
-        } elseif ($this->questionnaire_available) {
+        } else if ($this->questionnaireavailable) {
             $this->state = 1;
-        } elseif ($completed_q > 0) {
+        } else if (count( $this->usermanager->get_completed() ) > 0) {
             $this->state = 4;
         } else {
             $this->state = 2;
