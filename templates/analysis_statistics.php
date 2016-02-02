39ce4e795164584030001f735a5665a7d982e225
Rene Roepke <r.roepke@online.de>
2016-02-02 17:32:42 +0100

    langfiles fixed: no more spaces at the beginning or end

--- templates/analysis_statistics.php
+++ templates/analysis_statistics.php
@@ -22,7 +22,7 @@
 <div class="grid row_highlight">
     <div class="col_m_100"><?php echo get_string('are', 'groupformation');?>
         <b><?php echo $this->_['statistics_enrolled']; ?></b>
-        <?php echo ($this->_['statistics_enrolled']==1) ? get_string('students_available_single', 'groupformation') : get_string('students_available_multiple', 'groupformation'); ?></div>
+        <?php echo ' '.($this->_['statistics_enrolled']==1) ? get_string('students_available_single', 'groupformation') : get_string('students_available_multiple', 'groupformation'); ?></div>
 </div>
 <div class="grid row_highlight">
     <div class="col_m_100"><b><?php echo $this->_['statistics_processed']; ?></b>
@@ -30,15 +30,15 @@
 </div>
 <div class="grid row_highlight">
     <div class="col_m_100"><b><?php echo $this->_['statistics_submited']; ?></b>
-        <?php echo ($this->_['statistics_submited']==1) ? get_string('students_commited_single', 'groupformation') : get_string('students_commited_multiple', 'groupformation'); ?></div>
+        <?php echo ' '.($this->_['statistics_submited']==1) ? get_string('students_commited_single', 'groupformation') : get_string('students_commited_multiple', 'groupformation'); ?></div>
 </div>
 <div class="grid row_highlight">
 	<div class="col_m_100"><b><?php echo $this->_['statistics_submited_incomplete']; ?></b>
-        <?php echo get_string('commited_not_completed', 'groupformation');?></div>
+        <?php echo ' '.get_string('commited_not_completed', 'groupformation');?></div>
 </div>
 <div class="grid row_highlight">
     <div class="col_m_100"><?php echo get_string('are_now', 'groupformation');?> <b>
             <?php echo $this->_['statistics_submited_complete']; ?></b>
-        <?php echo get_string('completed_questionnaire', 'groupformation');?></div>
+        <?php echo ' '.get_string('completed_questionnaire', 'groupformation');?></div>
 </div>
 
