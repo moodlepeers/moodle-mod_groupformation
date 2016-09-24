d0707e3c2b39e2b2ed57cdf33040f7603cc7be91
Rene Roepke <r.roepke@online.de>
2016-09-24 19:45:10 +0200

    new table for user values

--- db/upgrade.php
+++ db/upgrade.php
@@ -1351,6 +1351,33 @@ function xmldb_groupformation_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2016092400, 'groupformation');
     }
 
+    if ($oldversion < 2016092401) {
+
+        // Define table groupformation_user_values to be created.
+        $table = new xmldb_table('groupformation_user_values');
+
+        // Adding fields to table groupformation_user_values.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('groupformationid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('criterion', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('label', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('dimension', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('value', XMLDB_TYPE_NUMBER, '20, 8', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table groupformation_user_values.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for groupformation_user_values.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Groupformation savepoint reached.
+        upgrade_mod_savepoint(true, 2016092401, 'groupformation');
+    }
+
+
 
     return true;
 }
