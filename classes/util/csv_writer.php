0a6ac001a3f639e06cadb7e08f65960d4bb77222
Rene Roepke <r.roepke@online.de>
2016-12-19 13:07:11 +0100

    csv writer edited

--- classes/util/csv_writer.php
+++ classes/util/csv_writer.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * This file contains a csv writer class
+ * This is a csv writer for exporting DB data
  *
  * @package mod_groupformation
  * @author Eduard Gallwas, Johannes Konert, Rene Roepke, Nora Wester, Ahmed Zukic
@@ -26,13 +26,6 @@ require_once($CFG->dirroot . '/mod/groupformation/classes/moodle_interface/user_
 require_once($CFG->dirroot . '/mod/groupformation/classes/moodle_interface/groups_manager.php');
 require_once($CFG->dirroot . '/mod/groupformation/classes/util/util.php');
 
-/**
- * This is a csv writer for exporting DB data
- *
- * @package     mod_groupformation
- * @copyright   2015 MoodlePeers
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 class mod_groupformation_csv_writer {
 
     /** @var cm_info */
@@ -54,13 +47,12 @@ class mod_groupformation_csv_writer {
     private $usermap = array();
 
     /** @var bool This determines whether the userids are replaced or not */
-    private $replaceuserids = true;
+    private $replaceuserids = false;
 
     /**
      * mod_groupformation_csv_writer constructor.
-     *
-     * @param cm_info $cm
-     * @param int $groupformationid
+     * @param $cm
+     * @param $groupformationid
      */
     public function __construct($cm, $groupformationid) {
         $this->cm = $cm;
@@ -74,8 +66,7 @@ class mod_groupformation_csv_writer {
 
     /**
      * Returns data by type
-     *
-     * @param string $type
+     * @param $type
      * @return string
      */
     public function get_data($type) {
@@ -95,8 +86,7 @@ class mod_groupformation_csv_writer {
 
     /**
      * Returns a csv-formatted string of a record
-     *
-     * @param stdClass $record
+     * @param $record
      * @param bool|false $title
      * @return string
      */
@@ -114,12 +104,12 @@ class mod_groupformation_csv_writer {
     /**
      * Returns a csv-formatted string of all records
      *
-     * @param array $records
+     * @param $records
      * @return string
      */
     public function records_to_csv($records) {
         $csv = null;
-        foreach (array_values($records) as $record) {
+        foreach ($records as $id => $record) {
             if (is_null($csv)) {
                 $csv = $this->record_to_csv($record, true) . "\n";
             }
@@ -196,24 +186,162 @@ class mod_groupformation_csv_writer {
         return $csv;
     }
 
-    /**
-     * Returns users for activity
-     *
-     * @return string
-     */
-    public function get_users() {
+    public function get_userids() {
+
+        $users = $this->usermanager->get_users_started('userid', 'userid');
         $groupusers = $this->groupsmanager->get_group_users('userid', 'userid,groupid,groupformation');
+
+        $us = array_values(array_keys($users));
+        $gs = array_values(array_keys($groupusers));
+
+        $users = array_values(array_merge($us, $gs));
+
+        $userids = array();
+        foreach ($users as $u) {
+            $userids[$u] = 1;
+        }
+
+        $users_current = array_keys($userids);
+
+        if ($this->groupformationid == 3 || $this->groupformationid == 4) {
+            $gid_orig = $this->groupformationid - 2;
+
+            $um_orig = new mod_groupformation_user_manager($gid_orig);
+            $gm_orig = new mod_groupformation_groups_manager($gid_orig);
+
+            $users = $um_orig->get_users_started('userid', 'userid');
+            $groupusers = $gm_orig->get_group_users('userid', 'userid,groupid,groupformation');
+
+            $us = array_values(array_keys($users));
+            $gs = array_values(array_keys($groupusers));
+
+            $users = array_values(array_merge($us, $gs));
+
+            $userids = array();
+            foreach ($users as $u) {
+                $userids[$u] = 1;
+            }
+
+            $users_orig = array_keys($userids);
+
+            $only_current = array();
+            $both_current = array();
+            foreach ($users_current as $user_current) {
+                if (!in_array($user_current, $users_orig)) {
+                    $only_current[] = $user_current;
+                }
+                if (in_array($user_current, $users_orig)) {
+                    $both_current[] = $user_current;
+                }
+            }
+
+            $only_orig = array();
+            $both_orig = array();
+
+            foreach ($users_orig as $us2u) {
+                if (!in_array($us2u, $users_current)) {
+                    $only_orig[] = $us2u;
+                }
+                if (in_array($us2u, $users_current)) {
+                    $both_orig[] = $us2u;
+                }
+            }
+
+            return $only_current;
+        }
+
+        return $users_current;
+    }
+
+    public function get_users() {
+        global $DB;
+
+        $us = $this->get_userids();
+
         $categories = $this->store->get_categories();
-        $users = array_keys($groupusers);
+
+        $unknown = array();
 
         $userdata = array();
-        foreach ($users as $userid) {
+        foreach ($us as $userid) {
             $userdata[$userid] = array();
+            $userdata[$userid]['code'] = $this->usermanager->get_participant_code($userid);
             $userdata[$userid]['groupformation'] = $this->groupformationid;
-            $userdata[$userid]['groupid'] = $groupusers[$userid]->groupid;
+
+            $userdata[$userid]['groupid'] = null;
+            $userdata[$userid]['groupid'] = null;
+            $userdata[$userid]['groupname'] = null;
+            $userdata[$userid]['performance_index'] = null;
+            $userdata[$userid]['groupkey'] = null;
+            $userdata[$userid]['rand'] = null;
+            $userdata[$userid]['mrand'] = null;
+            $userdata[$userid]['ex'] = null;
+            $userdata[$userid]['gh'] = null;
+
+            if ($this->groupsmanager->has_group($userid)) {
+                $groupid = $this->groupsmanager->get_group_id($userid);
+                $userdata[$userid]['groupid'] = $this->groupsmanager->get_moodle_group_id($groupid) - 394;
+                $userdata[$userid]['groupname'] = str_replace("G1_", "", $this->groupsmanager->get_group_name($userid));
+                $userdata[$userid]['performance_index'] = $this->groupsmanager->get_performance_index($groupid);
+
+                $groupkey = str_replace(';', '-', $this->groupsmanager->get_group_key($groupid));
+                $groupkey = str_replace('mrand', 'manual', $groupkey);
+
+                $userdata[$userid]['groupkey'] = $groupkey;
+
+                $manual = (strpos($groupkey, 'manual:'));
+                $random = (strpos($groupkey, 'rand:'));
+
+                $mrand = 0;
+                if ($manual !== false) {
+                    $mrand = substr($groupkey, strpos($groupkey, 'manual:') + 7, 1);
+                }
+
+                $rand = 0;
+                if ($random !== false) {
+                    $rand = substr($groupkey, strpos($groupkey, 'rand:') + 5, 1);
+                    $mrand = 0;
+                }
+
+                if ($rand == 1 || $mrand == 1) {
+                    $userdata[$userid]['performance_index'] = null;
+                }
+
+                $ex = substr($groupkey, strpos($groupkey, 'ex:') + 3, 1);
+                $gh = substr($groupkey, strpos($groupkey, 'gh:') + 3, 1);
+
+                $ex = str_replace('1', 1, $ex);
+                $ex = str_replace('_', 2, $ex);
+                $ex = str_replace('0', 3, $ex);
+
+                $gh = str_replace('1', 1, $gh);
+                $gh = str_replace('_', 2, $gh);
+                $gh = str_replace('0', 3, $gh);
+
+                $userdata[$userid]['rand'] = $rand;
+                $userdata[$userid]['mrand'] = $mrand;
+                $userdata[$userid]['ex'] = $ex;
+                $userdata[$userid]['gh'] = $gh;
+            } else {
+                $result = $DB->record_exists('groups_members',array('userid'=>$userid));
+                if ($result){
+                    $groupid = $DB->get_field('groups_members','groupid',array('userid'=>$userid));
+                    $courseid = $DB->get_field('groups','courseid',array('id'=>$groupid));
+                    if ($courseid == $this->groupformationid || $courseid + 2 == $this->groupformationid){
+                        //var_dump($userid . " in group ".($groupid - 394));
+                        $members = $DB->get_records('groups_members',array('groupid'=>$groupid),'userid','userid');
+                        $unknown = array_merge($unknown,array_keys($members));
+                        //var_dump(implode(', ',$unknown));
+                        $userdata[$userid]['groupid'] = $groupid - 394;
+                        $userdata[$userid]['random'] = 2;
+                    }
+                }
+            }
+
             foreach ($categories as $category) {
                 $userdata[$userid][$category] = array();
-                $answers = $this->usermanager->get_answers($userid, $category, 'questionid', 'questionid,answer');
+
+                $answers = $this->usermanager->get_answers($userid, $category, null, 'questionid, answer');
                 foreach ($answers as $answer) {
                     $questionid = $answer->questionid;
                     $userdata[$userid][$category][$questionid] = $answer->answer;
@@ -221,11 +349,44 @@ class mod_groupformation_csv_writer {
             }
         }
 
+        $unknown = array_values(array_unique($unknown));
+
+        foreach($unknown as $userid){
+            if (!array_key_exists($userid, $userdata)){
+                $userdata[$userid] = array();
+                $groupid = $DB->get_field('groups_members','groupid',array('userid'=>$userid));
+                $userdata[$userid]['groupid'] = $groupid - 394;
+                $userdata[$userid]['code'] = $this->usermanager->get_participant_code($userid);
+                $userdata[$userid]['groupformation'] = $this->groupformationid;
+                $userdata[$userid]['groupname'] = null;
+                $userdata[$userid]['performance_index'] = null;
+                $userdata[$userid]['groupkey'] = null;
+                $userdata[$userid]['rand'] = null;
+                $userdata[$userid]['mrand'] = null;
+                $userdata[$userid]['ex'] = null;
+                $userdata[$userid]['gh'] = null;
+                foreach ($categories as $category) {
+                    $userdata[$userid][$category] = array();
+                    $answers = $this->usermanager->get_answers($userid, $category, null, 'questionid, answer');
+                    foreach ($answers as $answer) {
+                        $questionid = $answer->questionid;
+                        $userdata[$userid][$category][$questionid] = $answer->answer;
+                    }
+                }
+            }
+        }
+
         $csv = "";
 
-        for ($j = 0; $j < count($users); $j++) {
+        //var_dump($us);
+        $us = array_values(array_unique(array_merge(array_values($us),array_values($unknown))));
+        //var_dump($us);
+        for ($j = 0; $j < count($us); $j++) {
+
             if ($j == 0) {
-                $csv .= "groupformationid,userid,groupid,";
+                $csv .= "userid,participantcode,groupformationid,groupid,groupname,performance_index,";
+                // $csv .= "groupkey,";
+                $csv .= "random,manual_random,criterion_extraversion,criterion_gewissenhaftigkeit,";
                 foreach ($categories as $category) {
                     if ($category == "knowledge" || $category == "topic") {
                         $temp = $this->store->get_knowledge_or_topic_values($category);
@@ -233,41 +394,58 @@ class mod_groupformation_csv_writer {
                         $options = mod_groupformation_util::xml_to_array($xmlcontent);
                         $csv .= implode(",", $options) . ",";
                     } else {
-                        $csv .= implode('_' . $category . ',', range(1, $this->store->get_number($category))) .
+                        $questions = $this->store->get_questions($category);
+                        $questionids = array();
+                        foreach ($questions as $question) {
+                            $questionids[] = $question->questionid;
+                        }
+
+                        $csv .= implode('_' . $category . ",", $questionids) .
                             '_' . $category . ",";
                     }
                 }
                 $csv = rtrim($csv, ",");
                 $csv .= "\n";
             }
-            $origuserid = $users[$j];
-            $userid = null;
-            if ($this->replaceuserids) {
-                $origuserid = $users[$j];
-                if (array_key_exists($origuserid, $this->usermap)) {
-                    $userid = $this->usermap[$origuserid];
-                } else {
-                    $next = count($this->usermap);
-                    $this->usermap[$origuserid] = $next;
-                    $userid = $next;
-                }
-            } else {
-                $userid = $users[$j];
-            }
-            $csv .= $userdata[$origuserid]['groupformation'] . ",";
-            $csv .= $userid . ",";
-            $csv .= $userdata[$origuserid]['groupid'] . ",";
+
+            $userid = $us[$j];
+
+            $line = "";
+            $line .= $userid . ",";
+            $line .= $userdata[$userid]['code'] . ",";
+            $line .= $userdata[$userid]['groupformation'] . ",";
+            $line .= $userdata[$userid]['groupid'] . ",";
+            $line .= $userdata[$userid]['groupname'] . ",";
+            $line .= $userdata[$userid]['performance_index'] . ",";
+            // $line .= $userdata[$userid]['groupkey'] . ",";
+            $line .= $userdata[$userid]['rand'] . ",";
+            $line .= $userdata[$userid]['mrand'] . ",";
+            $line .= $userdata[$userid]['ex'] . ",";
+            $line .= $userdata[$userid]['gh'] . ",";
+
             foreach ($categories as $category) {
-                $numberofquestions = $this->store->get_number($category);
-                $answers = $userdata[$origuserid][$category];
-                for ($i = 1; $i <= $numberofquestions; $i++) {
-                    if (array_key_exists($i, $answers)) {
-                        $csv .= $answers[$i] . ",";
-                    } else {
-                        $csv .= ",";
+
+                if ($category == "knowledge" || $category == "topic") {
+                    $optionscount = $this->store->get_number($category);
+
+                    for ($i = 1; $i <= $optionscount; $i++) {
+                        $line .= $this->usermanager->get_single_answer($userid, $category, $i) . ",";
+                    }
+                } else {
+                    $questions = $this->store->get_questions($category);
+
+                    foreach ($questions as $question) {
+                        $i = $question->questionid;
+                        if (array_key_exists($i, $userdata[$userid][$category])) {
+                            $line .= $userdata[$userid][$category][$i] . ",";
+                        } else {
+                            $line .= ",";
+                        }
                     }
                 }
             }
+
+            $csv .= $line;
             $csv = rtrim($csv, ",");
             $csv .= "\n";
         }
