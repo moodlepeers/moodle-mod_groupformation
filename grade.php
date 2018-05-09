5521c2bac2b16eb098ed6d7be76d1ae4d2438061
Rene Roepke <r.roepke@online.de>
2018-05-09 20:17:37 +0200

    grade fix

--- grade.php
+++ grade.php
@@ -21,7 +21,7 @@
  * @author Eduard Gallwas, Johannes Konert, Rene Roepke, Nora Wester, Ahmed Zukic
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-require_once(__DIR__ . "../../config.php");
+require_once(__DIR__ . "/../../config.php");
 
 $id = required_param('id', PARAM_INT);// Course module ID.
 
