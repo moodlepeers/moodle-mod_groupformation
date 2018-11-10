28c0e5a4285812f7d1e7c2070241d217a35ce806
Rene Roepke <r.roepke@online.de>
2018-11-10 22:14:53 +0100

    questionnaire update and reconfiguration

--- classes/util/define_file.php
+++ classes/util/define_file.php
@@ -42,7 +42,6 @@ class mod_groupformation_data {
                     "scenarios" => array(1, 2),
                     "evaluation" => true,
                     "labels" => array(
-                        // TODO discuss with Henrik which questions to use how.
                             "extraversion" => array(
                                     "scenarios" => array(1 => false, 2 => false),  // False = heterogeneous, True = homogeneous.
                                     "evaluation" => true,  // Use for displaying it to user (to compare to group and course).
@@ -52,28 +51,28 @@ class mod_groupformation_data {
                                     "cutoffs" => array(0.313169217, 0.776242547),
                             ),
                             "conscientiousness" => array(
-                                    "scenarios" => array(1 => true, 2 => true),
+                                    "scenarios" => array(1 => false, 2 => false),
                                     "evaluation" => true,
                                     "questionids" => array(-3, 8),
                                     "significant_id_only" => false,
                                     "cutoffs" => array(0.456596974, 0.831246163),
                             ),
                             "agreeableness" => array(
-                                    "scenarios" => array(1 => true, 2 => true),
+                                    "scenarios" => array(),
                                     "evaluation" => true,
                                     "questionids" => array(2, -7, 11),
                                     "significant_id_only" => false,
                                     "cutoffs" => array(0.492136484, 0.799889659),
                             ),
                             "neuroticism" => array(
-                                    "scenarios" => array(1 => false, 2 => false),
+                                    "scenarios" => array(),
                                     "evaluation" => true,
                                     "questionids" => array(-4, 9),
                                     "significant_id_only" => false,
                                     "cutoffs" => array(0.195135503, 0.602511556),
                             ),
                             "openness" => array(
-                                    "scenarios" => array(1 => false, 2 => false),
+                                    "scenarios" => array(),
                                     "evaluation" => true,
                                     "questionids" => array(-5, 10),
                                     "significant_id_only" => false,
@@ -89,7 +88,7 @@ class mod_groupformation_data {
                             "challenge" => array(
                                 // TODO: support scenariocriteriontypes and allow "none" to not use it for grouping.
                                 // Added for displaying feedback to user; no true/false asignment to prevent algorithmic usage.
-                                    "scenarios" => array(1 => false),
+                                    "scenarios" => array(),
                                     "evaluation" => true,
                                     "questionids" => array(6, 8, 10, 15, 17),
                                     "significant_id_only" => false,
@@ -111,7 +110,7 @@ class mod_groupformation_data {
                                     "cutoffs" => array(0.314297404, 0.511297834),
                             ),
                             "lackofconfidence" => array(
-                                    "scenarios" => array(1 => false),
+                                    "scenarios" => array(),
                                     "evaluation" => true,
                                     "questionids" => array(5, 9, 12, 16, 18),
                                     "significant_id_only" => false,
