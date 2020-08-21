680cb2413945e7e42125a620ff7303432a3f3325
Rene Roepke <r.roepke@online.de>
2020-08-21 17:13:55 +0200

    travis check4

--- classes/controller/grouping_controller.php
+++ classes/controller/grouping_controller.php
@@ -194,7 +194,7 @@ class mod_groupformation_grouping_controller {
 
     /**
      * POST action to delete generated and/or adopted groups (moodle groups)
-     * 
+     *
      * @param boolean $next
      */
     public function delete($next = false) {
